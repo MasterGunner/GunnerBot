f4d31cd16466e69e4ff3d718835c9ff004a4c761
MasterGunner <jimbob959@gmail.com>
2012-07-06 18:20:19 -0300

    V1.2

--- logger.rb
+++ logger.rb
@@ -1,1 +1,5 @@
-class Logger	def Logger.log (message)		File.new("LOG_#{Time.new.strftime("%Y-%m-%d")}.txt", "a").syswrite("#{Time.new.strftime("%H:%M:%S")}: #{message.chomp}\r\n")	endend
+class Logger
+	def Logger.log (message)
+		File.new("LOG_#{Time.new.strftime("%Y-%m-%d")}.txt", "a").syswrite("#{Time.new.strftime("%H:%M:%S")}: #{message.chomp}\r\n")
+	end
+end
