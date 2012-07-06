88a05e3c8b650877382f979b0f70f0a612954173
MasterGunner <jimbob959@gmail.com>
2012-07-05 22:44:46 -0300

    V1.2

--- RBot.rb
+++ RBot.rb
@@ -1,35 +1,25 @@
+require_relative 'logger'
+require 'thread'
 require 'socket'
 
-class IRC
-		@@s = nil
+class IRC < TCPSocket
+	alias :super_send :send;
 	def initialize (server, port, nick, chan)
-		@@server = server
-		@port = port
-		@nick = nick
-		@chan = chan
-	end
-	def connect
-		puts "Connecting..."
-		@@s = TCPSocket.open(@@server, @port)
+		super(server, port)
 		sleep(1)
-		send "NICK #{@nick}"
-		send "USER #{@nick} 8 *:Node.js IRC bot"
+		Logger.log "Connecting..."
+		send "NICK #{nick}"
+		send "USER #{nick} 8 *:Node.js IRC bot"
 		sleep(3)
-		send "JOIN #{@chan}"
-	end
-	def close
-		@@s.close
-	end
-	def read
-		return @@s.gets.chomp
+		send "JOIN #{chan}"
 	end
 	def send (message)
-		@@s.send "#{message}\r\n", 0
-		puts "SENT - #{message}"
+		super "#{message}\r\n", 0
+		Logger.log "SENT - #{message}"
 	end
 	def say (channel, message)
-		@@s.send "PRIVMSG #{channel} :#{data}\r\n"
-		puts "SENT(#{channel}) - #{data}"
+		super_send "PRIVMSG #{channel} :#{message}\r\n", 0
+		Logger.log "SENT(#{channel}) - #{message}"
 	end
 end
 
@@ -38,12 +28,25 @@ PORT = 6667
 NICK = "RubyGunner"
 CHAN = "#desertbus"
 
-iBot = IRC.new(SERVER, PORT, NICK, CHAN)
-iBot.connect
-while (line = iBot.read)
-	puts "RECV - " + line
-	if /^PING :(.+)$/i.match(line)
-		iBot.send ("PONG :" + $1)
+@iBot = IRC.new(SERVER, PORT, NICK, CHAN)
+def botRead
+	Logger.log "In Thread"
+	while (line = @iBot.gets)
+		Logger.log "RECV - " + line
+		if /^PING :(.+)$/i.match(line)
+			@iBot.send ("PONG :" + $1)
+		end
 	end
 end
-iBot.close
+def botWrite
+	while (line = gets.chomp)
+		if /^QUIT/.match(line)
+			break
+		end
+		@iBot.say("#DesertBus", "#{line}")
+	end
+end
+t1 = Thread.new{botRead}
+botWrite
+Thread.kill(t1)
+@iBot.close
---
+++ logger.rb
@@ -0,0 +1,1 @@
+class Logger	def Logger.log (message)		File.new("LOG_#{Time.new.strftime("%Y-%m-%d")}.txt", "a").syswrite("#{Time.new.strftime("%H:%M:%S")}: #{message.chomp}\r\n")	endend
