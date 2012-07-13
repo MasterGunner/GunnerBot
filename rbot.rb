0c3eee0fa11902fda9808d5a64bf8ea028c2df9d
MasterGunner <jimbob959@gmail.com>
2012-07-12 22:05:11 -0300

    V1.2

---
+++ rbot.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+=begin
+	IRC bot
+=end
+require_relative 'logger'
+require_relative 'irc'
+require_relative 'functions'
+require 'thread'
+
+#Connection variables
+SERVER = "irc.desertbus.org"
+PORT = 6667
+NICK = "RubyGunner"
+CHAN = "#desertbus"
+
+#Read from IRC Connection socket
+def botRead
+	Logger.log("In Thread")
+	while (line = @iBot.gets)
+		if /Gunner/i.match(line) || /(^| )MG($| )/i.match(line)
+			Logger.log "RECV - " + line
+			Functions.listeners(@iBot, line)
+		end
+	end
+end
+#Write to IRC Socket
+def botWrite
+	@chan = "#DesertBus"
+	while (line = gets.chomp)
+		if /^QUIT/.match(line)
+			break
+		elsif /^CHAN (.+)/.match(line)
+			@chan = $1
+		else
+			@iBot.say("#{@chan}", "#{line}")
+		end
+	end
+end
+
+#Connect to IRC
+@iBot = IRC.new(SERVER, PORT, NICK, CHAN)
+
+#Create instance of Read method in new Thread
+t1 = Thread.new{botRead}
+
+#Create instance of Write method in main Thread
+#Has access to terminal
+botWrite
+
+#Kill Read thread on exit
+Thread.kill(t1)
+
+#Close IRC connection
+@iBot.close
