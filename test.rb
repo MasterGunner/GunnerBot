d2115d74a54796bd999ab481390a47922765e575
MasterGunner <jimbob959@gmail.com>
2012-07-14 22:36:15 -0300

    V1.3

--- test.rb
+++ test.rb
@@ -1,32 +1,5 @@
-require_relative 'functions'
-
-def dieRoll(sides, mod)
-	total = Random.new.rand(1..Integer(sides))
-	if(!mod.empty?)
-		total = total + Integer(mod)
-	end
-	return total
-end
+require_relative 'testclass'
 
 while (line = ":MG!MG PRIVMSG #test :" + gets.chomp)
-	if /:(.*)!.* PRIVMSG (.*) :;Roll(Each)? (\d+)d(\d+) ?\+? ?(\d*)/i.match(line)
-		if (Integer($5) > 0 && Integer($5) < 1001 && Integer($4) > 0 && Integer($4) < 1001)
-			Logger.log "RECV - " + line
-			total = 0
-			rolls = []
-			if $3.nil?
-				for i in 1..Integer($4)
-					total = total + dieRoll($5, $6)
-				end
-				puts("#{$1} rolled: #{total}")
-			else
-				for i in 1..Integer($4)
-					rolls << dieRoll($5, $6)
-				end
-				puts("#{$1} rolled: #{rolls}")
-			end
-		else
-			puts("#{1.chr}ACTION Slaps #{$1}#{1.chr}")
-		end
-	end
+	Testclass.listeners(line)
 end
